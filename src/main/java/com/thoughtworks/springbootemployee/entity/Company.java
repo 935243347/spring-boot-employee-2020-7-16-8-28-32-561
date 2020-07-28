19e77b834d8f5071cfb05630b966010d4303b2ac
Max Miao <miaoma2@oocl.com>
2020-07-28 19:49:49 +0800

    [springboot-employee][max,front] feature: add Company entity

---
+++ src/main/java/com/thoughtworks/springbootemployee/entity/Company.java
@@ -0,0 +1,24 @@
+package com.thoughtworks.springbootemployee.entity;
+
+import java.util.List;
+
+public class Company {
+    private int id;
+    private List<Employee> employees;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public List<Employee> getEmployees() {
+        return employees;
+    }
+
+    public void setEmployees(List<Employee> employees) {
+        this.employees = employees;
+    }
+}
